18b22955412d97f5da453f01c92680ce131aa63f
dikkini <dikkini@gmail.com>
2019-03-29 19:54:19 +0300

    fix server geo pos

--- schema/data.sql
+++ schema/data.sql
@@ -6,11 +6,7 @@ TRUNCATE public.geo_position CASCADE;
 INSERT INTO public.vpnserversmeta(id, version, condition_version) VALUES (0, 0, 0);
 
 -- geo position
-INSERT INTO public.geo_position(id, latitude, longitude, country_code, state_code, city_id, region_common, region_dvd, region_xbox360, region_xboxone, region_playstation3, region_playstation4, region_nintendo) VALUES (1, 55.108, 54.039, 826, '1', 1, 0, 0, 0, 0, 0, 0, 0);
-INSERT INTO public.geo_position(id, latitude, longitude, country_code, state_code, city_id, region_common, region_dvd, region_xbox360, region_xboxone, region_playstation3, region_playstation4, region_nintendo) VALUES (2, 55.108, 54.039, 36, '3', 1, 0, 0, 0, 0, 0, 0, 0);
-INSERT INTO public.geo_position(id, latitude, longitude, country_code, state_code, city_id, region_common, region_dvd, region_xbox360, region_xboxone, region_playstation3, region_playstation4, region_nintendo) VALUES (3, 55.108, 54.039, 533, '4', 1, 0, 0, 0, 0, 0, 0, 0);
-INSERT INTO public.geo_position(id, latitude, longitude, country_code, state_code, city_id, region_common, region_dvd, region_xbox360, region_xboxone, region_playstation3, region_playstation4, region_nintendo) VALUES (4, 55.108, 54.039, 248, '5', 1, 0, 0, 0, 0, 0, 0, 0);
-
+INSERT INTO public.geo_position(id, latitude, longitude, country_code, state_code, city_id, region_common, region_dvd, region_xbox360, region_xboxone, region_playstation3, region_playstation4, region_nintendo) VALUES (1, 55.108, 54.039, 254, '1', 1, 0, 0, 0, 0, 0, 0, 0);
 --
 -- vpn server
 --
